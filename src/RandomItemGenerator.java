86a5d86a9e1c112979278311c0397aa578f57892
waramanand8759 <waramanand8759@7127R3S1.STUDENT.ad.fgcu.edu>
2018-09-27 09:38:27 -0400

    Test Commit

--- src/RandomItemGenerator.java
+++ src/RandomItemGenerator.java
@@ -1,20 +1,19 @@
 import java.util.Random;
 
 public class RandomItemGenerator {
-	
+
 	public String randomItemGen() {
 		int itemNumber = randomNumGen();
-		
+
 		return null;
 	}
-	
-	public int randomNumGen()
-	{
+
+	public int randomNumGen() {
 		int randomNumber;
 		Random rand = new Random();
-		
+
 		randomNumber = rand.nextInt(11);
-		
+
 		return randomNumber;
 	}
 
