c851d93f51d01d587f54e06a9b3f4f47e39b8b27
William Ramanand <waramanand8759@eagle.fgcu.edu>
2018-10-25 08:29:48 -0400

    Update BossEnemy

--- src/Character.java
+++ src/Character.java
@@ -14,6 +14,11 @@ public class Character {
    * char - a data type that stores a single character
    */
 
+  /*
+   * All variables below establish the data type for each and makes them private.
+   * Private means their scope is kept within this class and to access the variables
+   * you must use the getter or setter below.
+   */
   private String name;
   private int currentPower;
   private int maxPower;
@@ -21,9 +26,14 @@ public class Character {
   private double maxHealth;
   private char gender;
   private String item;
+  private String genderFull;
 
   public void setName(String newName) {
-    name = newName.substring(0, 1).toUpperCase() + newName.substring(1); // substring is one of
+    name = newName.substring(0, 1).toUpperCase() + newName.substring(1); // substring and toUpperCase are methods from the String class
+    
+    // This line makes sure the first letter of the person's name is capitalized
+    // It takes the first letter, capitalizes it, then adds the second letter
+    // and since there is no end index in the substring the rest of the String
   }
 
   public String getName() {
@@ -48,7 +58,7 @@ public class Character {
   }
 
   public double changeCurrentHealth(double changeCurHealth) {
-    currentHealth = currentHealth + changeCurHealth;
+    currentHealth += changeCurHealth;
 
     // This ternary operator insures that the currentHealth does not exceed the maxHealth
     currentHealth = (currentHealth > maxHealth) ? maxHealth : currentHealth;
@@ -56,7 +66,7 @@ public class Character {
   }
 
   public double changeMaxHealth(double changeMaxHealth) {
-    maxHealth = maxHealth + changeMaxHealth;
+    maxHealth += changeMaxHealth;
     return maxHealth;
   }
 
@@ -78,19 +88,29 @@ public class Character {
   }
 
   public int changeCurrentPower(int changeCurPower) {
-    currentPower = currentPower + changeCurPower;
+    currentPower += changeCurPower;
 
     currentPower = (currentPower > maxPower) ? maxPower : currentPower;
     return currentPower;
   }
 
   public int changeMaxPower(int changeMaxPower) {
-    maxPower = maxPower + changeMaxPower;
+    maxPower += changeMaxPower;
     return maxPower;
   }
 
   public void setGender(String newGender) {
-    gender = newGender.toUpperCase().charAt(0);
+    gender = newGender.toUpperCase().charAt(0); // charAt is the last of three methods from String class
+    
+ // This line capitalizes the M or F and converts from String
+ // to char
+    
+    // Below is the if/else statement
+    if (gender == 'M') { // == Operator compares both sides to see if they are the same, if they are its value is true, if not then false.
+    	genderFull = "Male";
+    } else if (gender == 'F') {
+    	genderFull = "Female";
+    }
   }
 
   public char getGender() {
--- src/ramanand/william/triton/enemy/BossEnemy.java
+++ src/ramanand/william/triton/enemy/BossEnemy.java
@@ -11,17 +11,10 @@ public class BossEnemy extends BasicEnemy {
   
   public BossEnemy() {
     Random r = new Random();
-    setName(grabRandomName(r.nextInt(17)) + " the ");
+    this.name = grabRandomName(r.nextInt(17)) + " the ";
     setEnemyPower(r.nextInt(1000) + 1);
     setEnemyHealth(r.nextInt(5000) + 1);
   }
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
   
 }
