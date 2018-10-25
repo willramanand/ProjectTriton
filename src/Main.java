6ee3b5ec4a0afca177131ecab9118eb2e5e7cfaf
willramanand <willramanand@gmail.com>
2018-10-25 09:17:26 -0400

    Reorganize main class and add Frame functionality

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.Canvas;
+import java.awt.Frame;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,31 +7,45 @@ import java.util.Scanner;
  * William Ramanand A sweet program to show off all I learned in COP 2006.
  */
 
-public class Main extends Canvas implements Runnable {
-
-<<<<<<< HEAD
+public class Main extends Canvas {
+  
   /**
    * 
    */
-  private static final long serialVersionUID = 8810007639119451188L;
-=======
->>>>>>> 34381f3d8e2bb4a92889c008da494f1a9ff50ffa
+  private static final long serialVersionUID = 1L;
+
+  static final int HEIGHT = 480;
+  static final int WIDTH = 640;
+  final static double VERSION = 1.0; // This is a final variable which means it has a constant value
   static int iCastUp = 2;
   static double dCastDown = 5.6;
   
-  @Override
-  public void run() {
-    // TODO Auto-generated method stub
-    
-  }
-
+  
   public static void main(String[] args) {
-    int mainMenuSelection;
-
+    
+    Frame frame = new Frame();
+    
+    frame.setVisible(false);
+    frame.setSize(WIDTH, HEIGHT);
+    frame.setResizable(false);
+    frame.setTitle("Integration Project");
+    frame.setVisible(true);
+    
+    
+    
+    
+    System.out.println("Welcome user to Project Triton\n" + "This is version " + VERSION); // Introduction
     printIntro();
 
+    }
+
+  public static void printIntro() {
+    
+    int mainMenuSelection;
+
     Scanner sc = new Scanner(System.in);
-<<<<<<< HEAD
+
+    
     System.out.println("_______Main Menu_______" + "\n1. Create a character!"
           + "\n2. Print random mathematical processes to prove my math skills!"
           + "\n3. Compare two inputted words!"
@@ -38,7 +53,7 @@ public class Main extends Canvas implements Runnable {
   
     System.out.println("Please enter a selection: ");
     mainMenuSelection = sc.nextInt();
-    while(mainMenuSelection != 4) {
+    
       switch (mainMenuSelection) {
         case 1:
           getCharacterInfo();
@@ -61,43 +76,7 @@ public class Main extends Canvas implements Runnable {
           System.out.println("That is not a valid choice!");
           break;
       }
-=======
-
-    System.out.println("_______Main Menu_______" + "\n1. Create a character!"
-        + "\n2. Print random mathematical processes to prove my math skills!"
-        + "\n3. Compare two inputted words!");
-
-    System.out.println("Please enter a selection: ");
-    mainMenuSelection = sc.nextInt();
-
-    switch (mainMenuSelection) {
-      case 1:
-        getCharacterInfo();
-        break;
-      case 2:
-        System.out.println(
-            "The integer " + iCastUp + " casted to a double is " + castIntToDouble(iCastUp)); 
-            // castIntToDouble is a method call and
-            // iCastUp is an argument within this method
-            // call
-        System.out.println(
-            "The double " + dCastDown + " cast to an integer is " + castDoubleToInt(dCastDown));
-        System.out.println(squareNum());
-        mathOps();
-        break;
-      case 3:
-        strComp();
-        break;
-      default:
-        System.out.println("That is not a valid choice!");
->>>>>>> 34381f3d8e2bb4a92889c008da494f1a9ff50ffa
-    }
     sc.close();
-  }
-
-  public static void printIntro() {
-    final double VERSION = 1.0; // This is a final variable which means it has a constant value
-    System.out.println("Welcome user to Project Triton\n" + "This is version " + VERSION); // Introduction
   }
 
   public static void getCharacterInfo() {
@@ -125,6 +104,7 @@ public class Main extends Canvas implements Runnable {
         newChar.getName(), newChar.getGender(), newChar.getCurrentHealth(), newChar.getMaxHealth(),
         newChar.getCurrentPower(), newChar.getMaxPower());
 
+    
   }
 
   // This method takes in int ICastUp and Cast it up to a double and returns double dCastUp
@@ -153,9 +133,7 @@ public class Main extends Canvas implements Runnable {
   /*
 <<<<<<< HEAD
    * Operator precedence determines the grouping of terms in an expression. This affects how an
-=======
    * Operator precedence determines the grouping of terms in an expression. \ This affects how an
->>>>>>> 34381f3d8e2bb4a92889c008da494f1a9ff50ffa
    * expression is evaluated. Certain operators have higher precedence than others; for example, the
    * multiplication operator has higher precedence than the addition operator
    */
@@ -178,10 +156,10 @@ public class Main extends Canvas implements Runnable {
     mathResult = 13 % 2;
     System.out.println("13 % 2 = " + mathResult);
 
-    mathResult = ++mathResult;
+    mathResult++;
     System.out.println("Incrementing the last result by 1 = " + mathResult);
 
-    mathResult = --mathResult;
+    mathResult--;
     System.out.println("Decrementing the last result by 1 = " + mathResult);
 
     mathResult += 10;
@@ -209,10 +187,7 @@ public class Main extends Canvas implements Runnable {
     } else {
       System.out.println("You did not enter the same word!");
     }
+    sc.close();
   }
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 34381f3d8e2bb4a92889c008da494f1a9ff50ffa
